ee9e7774b89ca085dacc3862861e054e3df0704e
Santiago-Czop <czopsantiago@gmail.com>
2020-11-04 17:06:59 -0300

    Añadi 3 graficos mas y un poco de logica para Opportunity Created

---
+++ Tp1/BySanti/graph_counter.py
@@ -0,0 +1,11 @@
+class Counter:
+    c = 1
+
+    def __init__(self):
+        return
+
+    def get_count(self):
+        return self.c
+
+    def increase_count(self):
+        self.c = self.c + 1
--- Tp1/BySanti/opportunity_created.py
+++ Tp1/BySanti/opportunity_created.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import numpy as np
 import seaborn as sns
 
 import filter
 from print import *
+import graph_counter
 
 original = pd.read_csv("file.csv")
 
@@ -15,74 +17,190 @@ columnas = len(original.columns)
 
 creation = original
 
-creation["Opportunity_Created_Year"] = creation["Opportunity_Created_Date"].apply(lambda x: x.year)
-creation["Opportunity_Created_Month"] = creation["Opportunity_Created_Date"].apply(lambda x: x.month)
-creation["Opportunity_Created_Day"] = creation["Opportunity_Created_Date"].apply(lambda x: x.day)
+creation["Opportunity_Created_Year"] = creation["Opportunity_Created_Date"].apply(
+    lambda x: x.year)
+creation["Opportunity_Created_Month"] = creation["Opportunity_Created_Date"].apply(
+    lambda x: x.month)
+creation["Opportunity_Created_Day"] = creation["Opportunity_Created_Date"].apply(
+    lambda x: x.day)
 
-#won = creation.loc[creation["Stage"] == "Closed Won", :]
-#won["Won_Per_Month"] = won.groupby("Opportunity_Created_Month")["Stage"].transform("count")
+# sns.displot(creation["Opportunity_Created_Month"])
 
-#closed = creation.loc[(creation["Stage"] == "Closed Won") | (creation["Stage"] == "Closed Lost"), :]  
-#closed["Closed_Per_Month"] = closed.groupby("Opportunity_Created_Month")["Stage"].transform("count")
+won = creation.loc[creation["Stage"] == "Closed Won", :]
+creation["Opportunity_Created_Year_Won"] = won["Opportunity_Created_Date"].apply(
+    lambda x: x.year)
+creation["Opportunity_Created_Month_Won"] = won["Opportunity_Created_Date"].apply(
+    lambda x: x.month)
+creation["Opportunity_Created_Day_Won"] = won["Opportunity_Created_Date"].apply(
+    lambda x: x.day)
 
-#won["Won_Per_Month_Normalized"] = won["Won_Per_Month"] / closed["Closed_Per_Month"]
-#won["Won_Per_Month_Normalized"].plot(kind="hist")
-#won_per_month_normalized = won.groupby("Opportunity_Created_Month")["Won_Per_Month_Normalized"]
-#plt.show()
-#print(won_per_month_normalized)
-#plt.figure(4)
-#plt.hist()
-#plt.show()
-#won_per_month_normalized.plot(y=)
+# creation = creation.merge(won, left_on="ID", right_on="ID", how="left")
+# print(creation["Opportunity_Created_Year"].value_counts())
+# print(creation["Opportunity_Created_Year_Won"].value_counts())
 
-#won_normalized = won["Won_Per_Month"].divide(closed["Closed_Per_Month"])
-#print(won_normalized.value_counts().to_string())
+# won["Won_Per_Month"] = won.groupby("Opportunity_Created_Month")["Stage"].transform("count")
 
-#print(creation.groupby("Opportunity_Created_Month")["Stage"].value_counts(normalize=True).reset_index())
+# closed = creation.loc[(creation["Stage"] == "Closed Won") | (creation["Stage"] == "Closed Lost"), :]
+# closed["Closed_Per_Month"] = closed.groupby("Opportunity_Created_Month")["Stage"].transform("count")
 
-#creation = creation.loc[(creation["Stage"] == "Closed Won") | (creation["Stage"] == "Closed Lost"), :]  
-#creation = creation.groupby("Opportunity_Created_Month")["Stage"].value_counts(normalize=True)
-#print(creation)
+# won["Won_Per_Month_Normalized"] = won["Won_Per_Month"] / closed["Closed_Per_Month"]
+# won["Won_Per_Month_Normalized"].plot(kind="hist")
+# won_per_month_normalized = won.groupby("Opportunity_Created_Month")["Won_Per_Month_Normalized"]
+# plt.show()
+# print(won_per_month_normalized)
+# plt.figure(4)
+# plt.hist()
+# plt.show()
+# won_per_month_normalized.plot(y=)
 
-def opportunity_year(df):
-    print(df["Opportunity_Created_Year"].value_counts().to_string())
+# won_normalized = won["Won_Per_Month"].divide(closed["Closed_Per_Month"])
+# print(won_normalized.value_counts().to_string())
+
+# print(creation.groupby("Opportunity_Created_Month")["Stage"].value_counts(normalize=True).reset_index())
+
+# creation = creation.loc[(creation["Stage"] == "Closed Won") | (creation["Stage"] == "Closed Lost"), :]
+# creation = creation.groupby("Opportunity_Created_Month")["Stage"].value_counts(normalize=True)
+# print(creation)
+
+
+def opportunity_created(df):
+    counter = graph_counter.Counter()
+
+    opportunity_year(df, counter)
+    opportunity_month(df, counter)
+    opportunity_day(df, counter)
+    opportunity_year_won(df, counter)
+    opportunity_month_won(df, counter)
+    opportunity_day_won(df, counter)
+
+
+def opportunity_year(df, counter):
     values = len(df["Opportunity_Created_Year"].value_counts())
-    print(f"values:{values}")
     bins = np.arange(2012, 2012 + values + 1) + 0.5
-    plt.figure(3)
+
+    plt.figure(counter.get_count())
     plt.hist(df["Opportunity_Created_Year"], bins, ec="black")
+
     plt.title("Oportunidades por Año")
     plt.xlabel("Año")
-    #plt.xticks([x for x in range(2013,2013+values+1)])
-    #plt.xlim(2013, values+1)
     plt.ylabel("Frecuencia")
+
     plt.savefig("graphics/year_hist.png")
 
-def opportunity_month(df):
-    print(df["Opportunity_Created_Month"].value_counts().to_string())
+    counter.increase_count()
+
+
+def opportunity_year_won(df, counter):
+    values = len(df["Opportunity_Created_Year"].value_counts())
+    bins = np.arange(2012, 2012 + values + 1) + 0.5
+    patch_blue = mpatches.Patch(color="blue", label="Oportunidades en Total")
+    patch_green = mpatches.Patch(color="green", label="Oportunidades Ganadas")
+
+    plt.figure(counter.get_count())
+    plt.hist(df["Opportunity_Created_Year"], bins, ec="black", color="blue")
+    plt.hist(df["Opportunity_Created_Year_Won"],
+             bins, ec="black", color="green")
+
+    plt.title("Oportunidades por Año Ganadas")
+    plt.xlabel("Año")
+    plt.ylabel("Frecuencia")
+
+    plt.legend(handles=[patch_blue, patch_green])
+
+    plt.savefig("graphics/year_won_hist.png")
+
+    counter.increase_count()
+
+
+def opportunity_month(df, counter):
     values = len(df["Opportunity_Created_Month"].value_counts())
     bins = np.arange(values + 1) + 0.5
+
+    plt.figure(counter.get_count())
     plt.hist(df["Opportunity_Created_Month"], bins, ec="black")
     plt.title("Oportunidades por Mes")
     plt.xlabel("Mes")
-    plt.xticks([x for x in range(1,values+1)])
-    plt.xlim(0, values+1)
     plt.ylabel("Frecuencia")
+
+    plt.xticks([x for x in range(1, values+1)])
+    plt.xlim(0, values+1)
+
     plt.savefig("graphics/months_hist.png")
 
-def opportunity_day(df):
-    print(df["Opportunity_Created_Day"].value_counts().to_string())
+    counter.increase_count()
+
+
+def opportunity_month_won(df, counter):
+    values = len(df["Opportunity_Created_Month"].value_counts())
+    bins = np.arange(values + 1) + 0.5
+
+    patch_blue = mpatches.Patch(color="blue", label="Oportunidades en Total")
+    patch_green = mpatches.Patch(
+        color="green", label="Oportunidades Ganadas")
+
+    plt.figure(counter.get_count())
+    plt.hist(df["Opportunity_Created_Month"], bins, ec="black")
+    plt.hist(df["Opportunity_Created_Month_Won"],
+             bins, ec="black", color="green")
+
+    plt.title("Oportunidades por Mes")
+    plt.xlabel("Mes")
+    plt.ylabel("Frecuencia")
+
+    plt.xticks([x for x in range(1, values+1)])
+    plt.xlim(0, values+1)
+
+    plt.legend(handles=[patch_blue, patch_green])
+
+    plt.savefig("graphics/months_won_hist.png")
+
+    counter.increase_count()
+
+
+def opportunity_day(df, counter):
     values = len(df["Opportunity_Created_Day"].value_counts())
     bins = np.arange(values + 1) + 0.5
-    plt.figure(2)
+
+    plt.figure(counter.get_count())
     plt.hist(df["Opportunity_Created_Day"], bins, ec="black")
+
     plt.title("Oportunidades por Día")
     plt.xlabel("Día")
-    plt.xticks([x for x in range(1,values+1)])
-    plt.xlim(0, values+1)
     plt.ylabel("Frecuencia")
+
+    plt.xticks([x for x in range(1, values+1)])
+    plt.xlim(0, values+1)
+
     plt.savefig("graphics/days_hist.png")
 
-#opportunity_year(creation)
-#opportunity_month(creation)
-#opportunity_day(creation)
+    counter.increase_count()
+
+
+def opportunity_day_won(df, counter):
+    values = len(df["Opportunity_Created_Day"].value_counts())
+    bins = np.arange(values + 1) + 0.5
+
+    patch_blue = mpatches.Patch(color="blue", label="Oportunidades en Total")
+    patch_green = mpatches.Patch(
+        color="green", label="Oportunidades Ganadas")
+
+    plt.figure(counter.get_count())
+    plt.hist(df["Opportunity_Created_Day"], bins, ec="black")
+    plt.hist(df["Opportunity_Created_Day_Won"],
+             bins, ec="black", color="green")
+
+    plt.title("Oportunidades por Día")
+    plt.xlabel("Día")
+    plt.ylabel("Frecuencia")
+
+    plt.xticks([x for x in range(1, values+1)])
+    plt.xlim(0, values+1)
+
+    plt.legend(handles=[patch_blue, patch_green])
+
+    plt.savefig("graphics/days_won_hist.png")
+
+    counter.increase_count()
+
+
+opportunity_created(creation)
