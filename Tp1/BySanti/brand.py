f043df3f6b5989da62489c32059d20ed57e43b7a
Santiago-Czop <czopsantiago@gmail.com>
2020-11-14 18:50:38 -0300

    Added analysis on Brand. Redid some of the graphs

--- Tp1/BySanti/brand.py
+++ Tp1/BySanti/brand.py
@@ -12,12 +12,78 @@ import graph_counter
 def brand():
     original = pd.read_csv("file.csv")
     filter.full_correction(original)
-
     set_output("brand_stats.txt")
-    print_series(original["Brand"].value_counts())
+
+    print_title("Estadísticas de la Marca")
+    brand_basic_prestats(original)
+
+    original = original.drop_duplicates(subset="Opportunity_Name")
+
+    brand_basic_stats(original)
+    brand_success(original)
+    brand_to_product_type_correlation(original)
+    product_type_to_brand_correlation(original)
 
     reset_output()
     return
+
+
+def brand_basic_prestats(df):
+    print_subtitle("Registros con Marca")
+    cant_registros = df["Brand"].count()
+    printt(f"{cant_registros}")
+    newline()
+
+
+def brand_basic_stats(df):
+    print_subtitle("Oportunidades con Marca")
+    cant_oport = df["Brand"].count()
+    printt(f"{cant_oport}")
+    newline()
+
+    print_subtitle("Cantidad de Marcas Distintas")
+    cant_marcas = len(df["Brand"].unique())
+    printt(f"{cant_marcas}")
+    newline()
+
+    print_subtitle("Cantidad de Oportunidades por Marca")
+    print_series(df["Brand"].value_counts())
+    newline()
+
+
+def brand_success(df):
+    df = df.groupby("Brand")
+
+    print_subtitle("Éxito según la Marca")
+    print_series(df["Stage"].value_counts())
+    newline()
+
+
+def product_type_to_brand_correlation(df):
+    df = df.groupby("Product_Type")
+
+    # Convertir -1 a other
+    print_subtitle("Marcas según Tipo de Producto")
+    print_series(df["Brand"].value_counts())
+    newline()
+
+    print_subtitle("Máxima Cantidad de Marcas por Tipo de Producto")
+    max_cantidad = df["Brand"].nunique().max()
+    printt(f"{max_cantidad}")
+    newline()
+
+
+def brand_to_product_type_correlation(df):
+    df = df.groupby("Brand")
+
+    print_subtitle("Tipos de Producto según Marcas")
+    print_series(df["Product_Type"].value_counts())
+    newline()
+
+    print_subtitle("Máxima Cantidad de Tipos de Producto por Marca")
+    max_cantidad = df["Product_Type"].nunique().max()
+    printt(f"{max_cantidad}")
+    newline()
 
 
 brand()
--- Tp1/BySanti/product_family.py
+++ Tp1/BySanti/product_family.py
@@ -107,9 +107,9 @@ def product_family_won(df, counter):
     values = len(df["Product_Family"].value_counts())
     bins = np.arange(values + 1) + 0.5
     patch_blue = mpatches.Patch(
-        color="#3333ff", label="Oportunidades en Total")
+        color="#ff9999", label="Oportunidades en Total")
     patch_green = mpatches.Patch(
-        color="#00bb00", label="Oportunidades Ganadas")
+        color="#99ff99", label="Oportunidades Ganadas")
 
     plt.figure(counter.get_count())
     #plt.hist(df["Product_Family"], bins, ec="black", color="blue")
@@ -118,14 +118,14 @@ def product_family_won(df, counter):
     #         bins, ec="black", color="green")
 
     plt.bar(df["Product_Family"].value_counts().index, df["Product_Family"].value_counts(),
-            width=0.7, align="center", color="#3333ff")
+            width=0.7, align="center", color="#ff9999")
     # plt.bar(df["Product_Family_Won"].value_counts().index, df["Product_Family_Won"].value_counts(),
     #        width=0.7, align="center")
 
     df = df.loc[df["Stage"] == "Closed Won", :]
 
     plt.bar(df["Product_Family"].value_counts().index,
-            df["Product_Family_Won"].value_counts(), width=0.7, align="center", color="#00bb00")
+            df["Product_Family_Won"].value_counts(), width=0.7, align="center", color="#99ff99")
 
     plt.title("Oportunidades por Familia de Producto Ganadas")
     plt.xlabel("Familia Producto")
